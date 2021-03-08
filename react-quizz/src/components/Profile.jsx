f7883d980d4ac24c03fe0711f7f57c34d35dbc39
Alex-95210 <kersulec.kersulec@gmail.com>
2021-03-08 17:36:43 +0100

    ajout du nb de games

--- react-quizz/src/components/Profile.jsx
+++ react-quizz/src/components/Profile.jsx
@@ -2,8 +2,7 @@ import React, { useRef, useSelector } from "react"
 import {Form, Container, Row, Col, Image, Card} from 'react-bootstrap'
 
 export default function Profile(props) {
-    console.log(props.src.games)
-
+    console.log(props.src)
     return (
 
             <Card style={{ width: '18rem' }}>
@@ -11,8 +10,8 @@ export default function Profile(props) {
                     <Card.Title>{props.src.name}</Card.Title>
                     <Card.Img variant="top" src={props.src.avatar} />
                     <Card.Text>
-
-                    {props.src.games}
+                    
+                    {props.src.nb_played_games}
                     </Card.Text>
                     <Card.Link href="#">Card Link</Card.Link>
                     <Card.Link href="#">Another Link</Card.Link>
